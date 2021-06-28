9ff0dfa8af234e763e6b2f3c1901b47709fa7864
AbelCodes247 <moltenabel@gmail.com>
2021-06-28 08:15:40 -0400

    Sting Slicing

---
+++ Google.py
@@ -0,0 +1,23 @@
+#mutiples = []
+#for x in range(1,11):
+    #mutiples.append(x*7)
+
+    #print(mutiples)
+
+#mutiples = [ x*7 for x in range(1,11)]
+#print(mutiples)
+
+#languages = ["Python", "Perl", "Ruby", "Go", "Java", "C"]
+#lenghts = [len(language) for language in languages]
+#print(lenghts)
+
+#squares = []
+#for x in range(1,6):
+    #squares.append(x**2)
+
+#print(squares)
+
+squares2 = [x**2 for x in range(1,6)]
+print(squares2)
+
+
